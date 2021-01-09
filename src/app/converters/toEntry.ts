f0defe7de6d026187371f784cf3ea875373d645e
bendikjohansen <bendikj@gmail.com>
2021-01-09 09:19:03 +0100

    fix: take date into consideration
    fix: outcome results

--- src/app/converters/toEntry.ts
+++ src/app/converters/toEntry.ts
@@ -2,12 +2,16 @@ import dayjs from "dayjs";
 import FormValues from "../../components/entryForm/FormValues";
 import { Entry } from "../types";
 
-const toEntry = (formEntry: FormValues): Entry => ({
-  id: null,
-  startTimestamp: dayjs(formEntry.from).unix(),
-  endTimestamp: dayjs(formEntry.to).unix(),
-  dayOff: formEntry.dayOff === 'true',
-  lunchDuration: formEntry.lunch,
-});
+const toEntry = (formEntry: FormValues): Entry => {
+  const start = dayjs(formEntry.from).date(dayjs(formEntry.date).date()).unix();
+  return {
+    id: null,
+    startTimestamp: start,
+    endTimestamp:
+      start + dayjs(formEntry.to).unix() - dayjs(formEntry.from).unix(),
+    dayOff: formEntry.dayOff === "true",
+    lunchDuration: formEntry.lunch,
+  };
+};
 
 export default toEntry;
